b507532687ad0eaff57493cf4d0a7ff2336d8aef
Duy Hiếu <heiuddtb01768@gmail.com>
2026-04-21 22:37:53 +0700

    admin dang lam tren main

--- obj/Debug/net9.0/KhoaHoc.AssemblyInfo.cs
+++ obj/Debug/net9.0/KhoaHoc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KhoaHoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9f80e13ed0cb409fdfbc01ae4d366e7830381b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c60133a8884ff970218d1ce424775ca94baeb9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("KhoaHoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KhoaHoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
